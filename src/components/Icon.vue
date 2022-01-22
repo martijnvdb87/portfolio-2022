f39efe4063164a3d58300d9dab26ab38d2513041
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-01-22 20:03:30 +0100

    Icon component mvp

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,12 +1,37 @@
 <script setup lang="ts">
-import icons from "./../icons";
+import { computed } from "vue";
+import icon from "./../icons";
+
+interface iconMap {
+    [name: string]: string
+}
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+const data = computed(() => (icon as iconMap)[props.name]);
 </script>
 
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-    {{ icons.play }}
-  </svg>
+  <svg
+    v-if="data"
+    class="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    role="img"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+    v-html="data"
+  ></svg>
 </template>
 
 <style lang="scss">
+.icon {
+  @apply flex items-center justify-center w-6 h-6 text-black;
+}
 </style>
