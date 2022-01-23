ff5d915497ea55f22f6ae7362f5651cf30f8d88c
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-01-24 00:04:55 +0100

    Start wotking on CodeBlock component

---
+++ src/components/CodeBlock.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import Prism from 'prismjs';
+
+import { ref, onMounted } from 'vue';
+
+const html = ref("");
+const input = ref(null);
+
+
+onMounted(() => {
+  if(input.value) {
+    const inputHtml: string = ((input.value as HTMLElement).children[0] as HTMLTextAreaElement).value;
+    html.value = Prism.highlight(inputHtml, Prism.languages.javascript, 'javascript');
+  }
+});
+
+</script>
+
+<template>
+<div class="code-block">
+  <div
+    v-if="!html"
+    class="code-block__input"
+    ref="input"
+  >
+    <slot />
+  </div>
+  <pre
+    v-if="html"
+    v-html="html"
+    class="code-block__output"
+  />
+</div>
+</template>
+
+<style lang="scss">
+.code-block__input {
+}
+.code-block__output {
+
+}
+</style>
