7e4eb898456c52d6be8aee8acff860d654f95e3a
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-01-31 20:31:09 +0100

    Added snippet search

---
+++ src/snippets/index.ts
@@ -0,0 +1,4 @@
+export default [{
+    title: "Test snippet",
+    content: "This is a test snippet."
+}]
--- src/views/Snippets.vue
+++ src/views/Snippets.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+import { ref, computed } from "vue";
+import snippets from "./../snippets";
+import fuzzysort from "fuzzysort";
+
+const search = ref("");
+
+const filteredSnippets = computed(() => search.value == "" ? snippets : fuzzysort.go(search.value, snippets, {key: "title"}).map(result => result.obj));
 </script>
 
 <template>
 <div>
+  <input type="text" v-model="search">
+  <ol>
+    <li v-for="(snippet, index) in filteredSnippets" :v-key="`snippet-${index}`">
+      {{snippet.title}}
+    </li>
+  </ol>
   Snippets
 </div>
 </template>
