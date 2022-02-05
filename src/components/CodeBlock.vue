c7c17a5d702a54ee1d18b4b3666a231ce065e780
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-02-05 16:16:34 +0100

    Added copy to clipboard button to CodeBlock

--- src/components/CodeBlock.vue
+++ src/components/CodeBlock.vue
@@ -3,6 +3,8 @@
 import './../scripts/prism.js';
 import './../styles/prism.scss';
 
+import Icon from "./Icon.vue";
+
 import { ref, onMounted } from 'vue';
 
 window.Prism.plugins.customClass.prefix('code-block__');
@@ -45,6 +47,10 @@ onMounted(() => {
   }
 });
 
+const renderedCode = ref("");
+
+const copyToClipboard = () => navigator.clipboard.writeText((renderedCode.value as unknown as HTMLElement).innerText);
+
 </script>
 
 <template>
@@ -65,17 +71,42 @@ onMounted(() => {
     v-if="html"
     v-html="html"
     class="code-block__output"
+    ref="renderedCode"
+    @click="copyToClipboard"
   />
+  <button
+    type="button"
+    class="code-block__copy-to-clipboard"
+  >
+    <Icon name="copy" />
+  </button>
 </div>
 </template>
 
 <style lang="scss" scoped>
 .code-block {
-    @apply text-gray-300 mb-6;
-}
-.code-block__input {
-}
-.code-block__output {
-  @apply bg-slate-700 px-10 py-8 rounded-lg overflow-auto;
+  @apply text-gray-300 mb-6 relative;
+  
+  &__input {}
+
+  &__output {
+    @apply bg-slate-700 px-10 py-8 rounded-lg overflow-auto;
+  }
+
+  .icon {
+      @apply duration-100 text-slate-400 w-4 h-4;
+  }
+
+  &__copy-to-clipboard {
+    @apply w-10 h-10 rounded-lg overflow-auto flex items-center justify-center absolute right-4 top-4 duration-100 transition-all;
+
+    &:hover {
+      @apply bg-slate-600;
+
+      .icon {
+      @apply text-slate-200;
+      }
+    }
+  }
 }
 </style>
